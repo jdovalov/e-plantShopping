86fe4a75cd309ce2641664b386c4e1a6f0fa3785
jef <jdovalov@gmail.com>
2025-01-06 20:49:08 -0500

    Implement CartSlice Reducers

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,30 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const { name, image, cost } = action.payload;
+        const existingItem = state.items.find(x=>x.name===name);
+        if ( existingItem ) {
+            existingItem.quantity++;
+        }
+        else
+        {
+            state.items.push({name,image,cost,quantity:1})
+        }
     },
     removeItem: (state, action) => {
+        const { name, image, cost } = action.payload;
+        state.items = state.items.filter(x=>x.name!==name);
     },
     updateQuantity: (state, action) => {
-
-    
+        const { name, quantity } = action.payload;
+        const existingItem = state.items.find(x=>x.name===name);
+        if (existingItem){
+            existingItem.quantity = quantity;
+        }
+        else
+        {
+            console.log(`Can't set quantity of {name}, item not in cart`);
+        }
     },
   },
 });
